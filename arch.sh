76bec12afbe344f134e7da0dd12aad6875361f5f
Sofian <sofian.v2@gmail.com>
2018-05-28 20:56:25 -0500

    More fixes

--- arch.sh
+++ arch.sh
@@ -24,24 +24,29 @@ sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 dialog --infobox "Installing System..." 4 40
-pacstrap /mnt base base-devel
+pacstrap /mnt base
+dialog --infobox "Done" 4 40
 
+dialog --infobox "Generating fstab..." 4 40
 genfstab -U /mnt >> /mnt/etc/fstab
 cat tz.tmp > /mnt/tzfinal.tmp
 rm tz.tmp
 
+dialog --infobox "Generating lang and host file..." 4 40
 hostname=$(cat comp)
 cat comp > /mnt/etc/hostname && rm comp
 
-echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t""$hostname"".localdomain ""$hostname" > hosts
+echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t""$hostname"".localdomain ""$hostname" > /mnt/etc/hosts
 
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
 locale-gen
 
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
-cp chroot.sh /mnt/chrot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
+dialog --infobox "Chroot into system, setup clock, and install bootloader..." 4 40
+cp chroot.sh /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
 
+dialog --infobox "Done!" 4 40
 mount -R /mnt
 
 dialog --defaultno --title "Final Q" --yesno "Reboot computer?"  5 30 && reboot
--- chroot.sh
+++ chroot.sh
@@ -5,5 +5,6 @@ TZuser=$(cat tzfinal.tmp)
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 hwclock --systohc
 
+dialog  --infobox "Installing bootloader" 4 40
 pacman --noconfirm --needed -S grub efibootmgr intel-ucode && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux && grub-mkconfig -o /boot/grub/grub.cfg
 

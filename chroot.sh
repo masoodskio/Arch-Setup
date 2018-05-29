6dd3ec60b572716c2d83989783a2645661b8a2c6
Sofian <sofian.v2@gmail.com>
2018-05-29 02:51:51 -0500

    Modified grub default

--- chroot.sh
+++ chroot.sh
@@ -1,3 +1,4 @@
+pacman --noconfirm --needed -S dialog
 passwd
 
 TZuser=$(cat tzfinal.tmp)
@@ -6,6 +7,13 @@ rm tzfinal.tmp
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 hwclock --systohc
 
-dialog  --infobox "Installing bootloader" 4 40
-pacman --noconfirm --needed -S iw wpa_supplicant dialog grub efibootmgr intel-ucode dosfstools os-prober && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux && grub-mkconfig -o /boot/grub/grub.cfg
+dialog  --infobox "Installing bootloader and other important utils" 4 40
+pacman --noconfirm --needed -S zsh grml-zsh-config iw wpa_supplicant grub efibootmgr intel-ucode dosfstools os-prober vim ntfs-3g
 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux
+
+sed -i 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="acpi_osi='\''!Windows 2012'\''"/' /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+chsh -s /bin/zsh

8a83371675f16571976041389c836caaa30246de
Sofian <sofian.v2@gmail.com>
2018-05-29 00:32:39 -0500

    Added support for wifi-menu

--- chroot.sh
+++ chroot.sh
@@ -7,5 +7,5 @@ ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 hwclock --systohc
 
 dialog  --infobox "Installing bootloader" 4 40
-pacman --noconfirm --needed -S grub efibootmgr intel-ucode && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux && grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm --needed -S iw wpa_supplicant dialog grub efibootmgr intel-ucode dosfstools os-prober && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux && grub-mkconfig -o /boot/grub/grub.cfg
 

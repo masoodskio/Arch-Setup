7e7c9cc2f497a58fca61b396dc6d0412709dd7cf
Sofian <sofian.v2@gmail>
2018-07-24 22:32:13 -0500

    Cleaned up the script a little bit. Added new utilites

--- arch.sh
+++ arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #This is a script to make installing Arch Linux easier for me.
-#Feel free to use it however this is provided without any warrenties
+#Feel free to use it however this is provided without any warranties
 
 pacman -Sy
 pacman -S --noconfirm dialog || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
@@ -14,15 +14,11 @@ dialog --defaultno --title "Partition/Mount HD" --yesno "Have you partitioned an
 
 dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
 
-dialog --defaultno --title "Time Zone select" --yesno "Do you want use the default time zone(America/Chicago)?.\n\nPress no for select your own time zone"  10 60 && echo "America/Chicago" > tz.tmp || tzselect > tz.tmp
+dialog --defaultno --title "Time Zone select" --yesno 
+"Do you want use the default time zone(America/Chicago)?.\n\nPress no for select your own time zone" 
+10 60 && echo "America/Chicago" > tz.tmp || tzselect > tz.tmp
 
 timedatectl set-ntp true
-
-dialog --infobox "Updating mirrorlist, this may take a while..." 4 40
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-#rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-
 dialog --infobox "Installing System..." 4 40
 pacstrap /mnt base base-devel
 dialog --infobox "Done" 4 40
@@ -37,11 +33,6 @@ hostname=$(cat comp)
 cat comp > /mnt/etc/hostname && rm comp
 
 echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t""$hostname"".localdomain ""$hostname" > /mnt/etc/hosts
-
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
-locale-gen
-
-echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
 dialog --infobox "Chroot into system, setup clock, and install bootloader..." 4 40
 cp chroot.sh /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
--- chroot.sh
+++ chroot.sh
@@ -7,20 +7,20 @@ rm tzfinal.tmp
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 hwclock --systohc
 
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
 dialog  --infobox "Installing bootloader and other important utils" 4 40
-pacman --noconfirm --needed -S zsh grml-zsh-config iw wpa_supplicant grub efibootmgr intel-ucode dosfstools os-prober vim ntfs-3g feh
-pacman --noconfirm --needed -S pulseaudio pulseaudio-alsa pamixer ponymix alsa-utils mesa ttf-dejavu xorg-server xorg-xset xorg-xinit unrar unzip w3m wget git exfat-utils
+pacman --noconfirm --needed -S zsh grml-zsh-config iw wpa_supplicant grub neovim efibootmgr intel-ucode dosfstools os-prober ntfs-3g exfat-utils
+pacman --noconfirm --needed -S git pulseaudio pulseaudio-alsa pamixer ponymix alsa-utils ttf-dejavu xorg-server xorg-xset xorg-xinit mesa xf86-video-intel unrar unzip w3m wget feh
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux
-
 sed -i 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="acpi_osi='\''!Windows 2012'\''"/' /etc/default/grub
-
 grub-mkconfig -o /boot/grub/grub.cfg
 
 chsh -s /bin/zsh
-
 sed -i 's/bash/zsh/' /etc/default/useradd
-
 
 name=$(dialog --no-cancel --inputbox "Enter a username for the user account." 10 60 3>&1 1>&2 2>&3 3>&1)
 
